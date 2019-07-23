785061dc25d67daddd59944a1aedf1316c195c1f
Sven Glawion <glawion@posteo.de>
2019-07-23 08:42:09 +0200

    Replace functions for Interrupts with inline function in header.

--- uart.h
+++ uart.h
@@ -39,6 +39,8 @@
 
 #define nop() __asm volatile ("nop")
 
+extern volatile uint16_t tx_data, tx_data_ref;
+extern volatile uint8_t bit_count, rx_data;
 extern volatile uint8_t uart_status;
 
 #ifdef USE_FIFO
@@ -110,13 +112,113 @@ _inline_fifo_get (fifo_t *f)
 }
 
 #endif // USE_FIFO
+// send each bit
+static inline void tim1_compa_func(){
+    // for collision management the sent bit should be read in and compared with read byte.
+    // if there is a mismatch -> collision
+    uint16_t tx_data_tmp = tx_data;
+    //uint16_t rx_data_tmp = rx_data;
 
-void uart_init();
-void uart_putc(uint8_t c);
-void tim1_compa_func();
-void tim1_compb_func();
-void pcint0_func();
-uint8_t uart_get();
-uint8_t uart_get_wait();
+    // disable interupt flag on end of transmission
+    if (tx_data_tmp & 1L){
+        SUART_TXD_PORT |=  (1 << SUART_TXD_BIT);
+        SUART_TXD_PORT |=  (1 << PA0);
+    }
+    else{
+        SUART_TXD_PORT &= ~(1 << SUART_TXD_BIT);
+        SUART_TXD_PORT &= ~(1 << PA0);
+    }
+    tx_data_tmp >>= 1;
+
+    // read RX for checking write collision
+    //if (SUART_RXD_PIN & (1 << SUART_RXD_BIT))
+    //    rx_data_tmp |= (1 << 10);
+    //tx_data_tmp >>= 1;
+
+    if (tx_data_tmp == 1L){
+        //if(tx_data_tmp != rx_data)
+        //    uart_status |= UART_STATUS_COLLISION;
+        uart_status |= UART_STATUS_TX_DONE;
+        uart_status &= ~UART_STATUS_ACTIVE;
+        TIMSK1 &= ~(1 << OCIE1A);
+        SUART_TXD_PORT &= ~((1 << PA0)|(1 << SUART_TXD_BIT));
+        GIMSK |= (1<<PCIE0);
+        return;
+    }
+
+    tx_data = tx_data_tmp;
+    //rx_data = rx_data_tmp;
+}
+
+
+/* receive bits
+ * abtasten immer auf zeitintervall halbe
+ */
+static inline void pcint0_func()
+{
+   // Pin change interupt beenden
+    GIMSK &= ~(1<<PCIE0);
+    GIFR = (1<<PCIF0);
+
+    uint16_t tcnt1 = TCNT1;
+    uint16_t ocr1a = OCR1A;
+   
+
+    // Compare match a value and activate interrupt 
+    // Eine halbe Bitzeit zu ICR1 addieren (modulo OCR1A) und nach OCR1B
+    uint16_t ocr1b;
+    ocr1b = tcnt1 + ocr1a/2;
+    if (ocr1b >= ocr1a)
+        ocr1b -= ocr1a;
+    OCR1B = ocr1b;
+    
+    TIMSK1 |= (1 << OCIE1B);
+    
+    // clear in data
+    rx_data = 0;
+    uart_status &= ~((1<<UART_STATUS_ACTIVE)|(1<<UART_STATUS_RX_DONE));
+    bit_count = 0;
+}
+
+static inline void tim1_compb_func(){
+    uint8_t rx_data_tmp = rx_data;
+    uint8_t bit_count_tmp = bit_count;
+    // shift to make space for new bit
+    rx_data_tmp >>= 1;
+    
+    // set vaule 1 if pin is hight
+    if (SUART_RXD_PIN & (1 << SUART_RXD_BIT)){
+        //rx_data_tmp+=1;
+        rx_data_tmp |= (1 << 7);
+        SUART_TXD_PORT |=  (1 << PA0);
+    }
+    else
+        //  toggle lede to show activity (debug)
+        SUART_TXD_PORT &=  ~(1 << PA0);
+
+    bit_count_tmp++;
+   
+    if (bit_count_tmp == 9){
+        // disable Combare Match A and enable RX interupts
+        TIMSK1 &= ~(1 << OCIE1B);
+        //if(rx_data_tmp > 4)
+        //    SUART_TXD_PORT |=  (1 << PA0);
+        //else
+        SUART_TXD_PORT &=  ~(1 << PA0);
+        // set uart_status
+        uart_status &= ~(1 << UART_STATUS_RX_DONE);
+        uart_status |= (1 << UART_STATUS_RX_DONE);
+        // enable rx interrupts
+        GIMSK  |=  (1 << PCIE0);
+        GIFR = (1<<PCIF0);
+    } 
+    rx_data = rx_data_tmp;
+    bit_count = bit_count_tmp;
+}
+
+extern void uart_init();
+extern void uart_putc(uint8_t c);
+extern uint8_t uart_get();
+extern uint8_t uart_get_wait();
 #endif // __UART_H__
 
